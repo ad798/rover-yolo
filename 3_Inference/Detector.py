c97005f12fcaaadf8b8c55cb45454f16099d4415
Alan Quimbita <43886968+ad798@users.noreply.github.com>
2021-03-12 12:52:55 -0500

    Update Detector.py

--- 3_Inference/Detector.py
+++ 3_Inference/Detector.py
@@ -138,8 +138,8 @@ if __name__ == "__main__":
         "--postfix",
         type=str,
         dest="postfix",
-        default="_catface",
-        help='Specify the postfix for images with bounding boxes. Default is "_catface"',
+        default="_oidio",
+        help='Specify the postfix for images with bounding boxes. Default is "_oidio"',
     )
 
     parser.add_argument(
